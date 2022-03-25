cffa9d653e1e079aa32a3913ef7b13dcf619f1f8
bigmurtaza <muratcansarkalkan@gmail.com>
2022-03-26 01:21:05 +0300

    Updated indeed function

--- indeed.py
+++ indeed.py
@@ -2,6 +2,12 @@ class Indeed():
 
     def parser(soup):
         # Finds job title
-        job_title = soup.find_all(class_='jobTitle')
+        job_title = soup.find_all(class_="icl-u-xs-mb--xs icl-u-xs-mt--none jobsearch-JobInfoHeader-title")
+        for job in job_title:
+            print(job.text)
+        
+        company_title = soup.find_all(class_="icl-u-lg-mr--sm icl-u-xs-mr--xs")
+        for company in company_title:
+            print(company.text)
 
-        print(job_title)
+        
